1e416f722467cc850738962331d7f4b3d6b2bfdc
= <alitunacanonar@icloud.com>
2021-05-11 01:46:55 +0300

    install livewire

---
+++ app/Http/Livewire/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HelloWorld extends Component
+{
+    public function render()
+    {
+        return view('livewire.hello-world', [
+            'name' => 'Ali Onar'
+        ]);
+    }
+}
---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Hello from Livewire Component</h1>
+    <p>My name is {{ $name }}</p>
+</div>
